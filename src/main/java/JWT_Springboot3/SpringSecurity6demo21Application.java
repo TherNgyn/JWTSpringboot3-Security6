281e46d7774972c69c387b415a07708d22cebd3b
Nguyễn Thị Hồng Thơ <22151305@student.hcmute.edu.vn>
2024-11-21 17:01:36 +0700

    first commit

---
+++ src/main/java/JWT_Springboot3/SpringSecurity6demo21Application.java
@@ -0,0 +1,13 @@
+package JWT_Springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6demo21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6demo21Application.class, args);
+	}
+
+}
---
+++ src/test/java/JWT_Springboot3/SpringSecurity6demo21ApplicationTests.java
@@ -0,0 +1,13 @@
+package JWT_Springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6demo21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
